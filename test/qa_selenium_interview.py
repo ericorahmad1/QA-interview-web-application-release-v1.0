f2cc4f83042b49ccd14cccbf533fbdd4ca3f7ac8
Ericorahmad1 <ericorahmad1@gmail.com>
2021-12-29 17:33:01 +0800

    update code

--- test/qa_selenium_interview.py
+++ test/qa_selenium_interview.py
@@ -1,32 +1,37 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 class QaInterviewTest(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        options = Options()
+        options.add_argument("start-maximized")
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options )
 
     def test_qa_selenium_interview(self):
         driver = self.driver
         driver.get("localhost:6464")
         # self.assertIn("factorial", driver.title)
-        
-        heading1 = driver.find_element_by_tag_name('h1')
-        
+        # heading1 = driver.find_element_by_tag_name('h1')
         number = 5
-        element = driver.find_elements_by_name(self, "number")
-        button = driver.find_elements_by_id(self, "getFactorial")
+        # element enter integer number field
+        element = driver.find_elements_by_id("number")
+        # button factorial
+        button = driver.find_elements_by_name("getFactorial")
+        # send number
         element.send_keys(number)
         element.send_keys(Keys.RETURN)
         assert "No results found." not in driver.page_source
         button.click()
 
-        def negative_test_case():
+    # def negative_test_case(self):
 
+    # def empty_test_case(self):
 
-        def nu
 
     def tearDown(self):
         self.driver.close()
